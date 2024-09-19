cf87ef2ca369357bca41b004c9321a150ef28507
glove <glovenone@gmail.com>
2024-09-19 10:24:41 +0800

    add files

---
+++ server_quant.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const targetUrl = 'http://43.163.200.28:8090';
+
+// 打印所有请求
+app.use((req, res, next) => {
+	console.log(`Request: ${new Date().toISOString()} - [${req.method}] ${req.originalUrl}`);
+	next(); // 确保请求可以继续进行到下一个中间件或路由
+});
+
+// 代理配置
+const options = {
+	target: targetUrl, // 目标主机
+	changeOrigin: true, // 需要虚拟托管站点
+	pathRewrite: {
+		'^/': '',
+	},
+	onProxyReq: (proxyReq, req, res) => {
+		console.log(`[Proxy] ${req.method} ${req.path}`);
+	},
+};
+
+// 使用中间件
+app.use('/', createProxyMiddleware(options));
+
+// 监听端口
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
+
